21dbb36bd7e0e0fc01b71f2698021b6a980753f1
webdesignoko <55218006+webdesignoko@users.noreply.github.com>
2019-09-12 05:05:18 +0100

    Add files via upload

---
+++ assert/js/apps.js
@@ -0,0 +1,26 @@
+
+// $(document).ready(function() {
+
+
+//     $(".hamburger-icon").on("click",  function() {
+//         if($(  ".main-nav"  ).hasClass("mobile-hide"  )){
+//             $(".main-nav").removeClass("moble-hide");
+//             $(".main-nav").addClass("mobile-show");            
+//         }else{
+//             $(".main-nav").addClass("mobile-hide");
+//             $(".main-nav").removeClass("mobile-show");
+//         }
+
+//     });
+
+
+// });
+
+function noSearch() {
+    return alert('No Search Available now.');
+    
+}
+
+function noMessage () {
+    return alert('information is not acceptable.')
+}
